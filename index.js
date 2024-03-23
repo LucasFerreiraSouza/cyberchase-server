c227bdbb9cb86766d5f8a5f4f7af448194ac306b
Daniel Bispo <danielbispo3015@hotmail.com>
2024-03-23 12:05:51 -0300

    Organizando rotas de person

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = express.Router();
-const Person = require("./models/Person");
-//import { Person } from '../models/Person';
-
+const personRoutes = require('./routes/person-routes')
 
 const app = express();
 app.use(
     express.urlencoded({extended:true})
 )
 app.use(express.json());
+app.use('/person', personRoutes)
+
 app.listen(8080);
-
-app.post('/person', async (req, res) => {
-    const { name, salary, approved } = req.body || {}
-  
-    if (!name) res.status(422).json({ message: 'Erro: É obrigatório enviar o nome.' })
-    if (!salary) res.status(422).json({ message: 'Erro: É obrigatório enviar o salário.' })
-  
-    try {
-      await Person.create({ name, salary, approved })
-  
-      res.status(201).json({ message: 'Novo usuário cadastrado com sucesso!' })
-    } catch (error) {
-      res.status(500).json({ error })
-    }
-  })
-
 
 
 mongoose.connect(
