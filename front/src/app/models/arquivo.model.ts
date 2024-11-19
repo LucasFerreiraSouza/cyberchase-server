0feb0aeb2490357c91883e3c4bbdd164fc394bdd
DANIEL BISPO <0030482123005@lti.fatecso.edu.br>
2024-11-19 20:06:12 -0300

    v1

--- front/src/app/models/arquivo.model.ts
+++
@@ -1,8 +0,0 @@
-export class ArquivoModel {
-  id?: any;
-  nome?: string;
-  base64?: string; // Atualize este campo para armazenar os dados em Base64
-  createdAt?: any; // ou Date, dependendo do formato que você recebe do servidor
-  
-  pergunta?: string;
-}
